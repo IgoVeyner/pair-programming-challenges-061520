de537d486e179fbb1dab6f6693024d290f0f06f1
IgoVeyner <igorveyner95@gmail.com>
2020-08-28 16:50:46 -0400

    add solution

--- 04-active-record-agents/app/models/organization.rb
+++ 04-active-record-agents/app/models/organization.rb
@@ -1,4 +1,17 @@
 # In order for this to work, Organization needs to inherit from something...
-class Organization
+class Organization < ActiveRecord::Base
+  has_many :agents
 
+  def active_agents
+    self.agents
+  end
+
+  def erase
+    self.agents.each {|a| a.destroy}
+    self.destroy
+  end
+
+  def older_organizations
+    Organization.where("year_formed < #{self.year_formed}")
+  end
 end
